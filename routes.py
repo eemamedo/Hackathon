0eb985c4e8845dbb09fd5ff6d5ffa25395bb74bc
Arie Field <arie.field@gmail.com>
2019-05-09 10:59:16 -0400

    Update routes

--- routes.py
+++ routes.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return "API is running"
 
-@app.route('/model')
-def model():
-    return "Model result"
+@app.route('/sensorData')
+def sensorData():
+    payload = []
+    payload.append({
+        "Time": "2017-08-23 0:00",
+        "Power": 1989,
+        "Voltage": 555
+    })
+    payload.append({
+        "Time": "2017-08-23 0:10",
+        "Power": 1822,
+        "Voltage": 560
+    })
+    payload.append({
+        "Time": "2017-08-23 0:20",
+        "Power": 1800,
+        "Voltage": 540
+    })
+    return jsonify(payload)
+
+
 
 if __name__ == '__main__':
     app.run()
