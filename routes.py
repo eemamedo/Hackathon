7db6c10b7f1848ab8f6bb18e5d24e8d37405c4ec
Arie Field <arie.field@gmail.com>
2019-05-09 13:41:02 -0400

    fake indices

--- routes.py
+++ routes.py
@@ -1,8 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import mongo.mongoWrapper as mw
 app = Flask(__name__)
 CORS(app)
+
+# SCR_PATH = os.path.join('docs', 'SCR')
+
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = SCR_PATH
+
+# @app.route('/')
+# @app.route('/index')
+# def show_index():
+#     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
+#     return render_template("index.html", user_image = full_filename)
 
 @app.route('/')
 def root():
@@ -34,19 +45,23 @@ def sensorData():
     currentList = []
 
     f = open("credentials.txt", "r")
-    password = f.read()
+    password = f.read()                                                         
     db = mw.getDBRef(password, "FinalTimeData")
 
     col = mw.queryCol(db, "SensorReadings")
-    for doc in col:
-        powerList.append({'x':doc['Time'], 'y': doc['Power']})
-        currentList.append({'x':doc['Time'], 'y': doc['Current']})
+    for i, doc in enumerate(col):
+        powerList.append({'x':i, 'y': doc['Power']})
+        currentList.append({'x':i, 'y': doc['Current']})
 
     payload = {}
     payload['powerList'] = powerList
     payload['currentList'] = currentList
         
     return jsonify(payload)
+
+@app.route('/scr')
+def scr():
+    return render_template('scr.html', name='scr')
 
 
 if __name__ == '__main__':
